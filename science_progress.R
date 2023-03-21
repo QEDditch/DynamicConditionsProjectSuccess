88aff608782224044a277f0f3470e55bb16d1605
David <david.eggleton@gmail.com>
2023-03-21 16:30:31 +0000

    Science progress stats included now

---
+++ science_progress.R
@@ -0,0 +1,26 @@
+library(readr)
+library(tidyr)
+library(tidyverse)
+library(dplyr)
+library(tm)
+library(wordcloud2)                
+library(networkD3)
+
+mybib = read_csv("science progress bibliometrics.csv")
+data.frame(colnames(mybib))
+dim(mybib)
+
+mybib %>% 
+  filter(`Item Type` == "journalArticle") %>% 
+  group_by(`Publication Title`) %>% 
+  tally(sort = TRUE) %>% 
+  mutate(Percentage = 100*(n / sum(n))) %>% 
+  rename("Number of Studies" = n) %>% 
+  rename("Publication Title" = `Publication Title`) %>% 
+  write_csv(file = "Journal Counts.csv")
+
+
+mybib$`Publication Year` <- as.integer(mybib$`Publication Year`)
+p <- ggplot(mybib, aes(x = `Publication Year`))
+p + geom_histogram(binwidth = 5, fill = "blue")
+ggsave("articles by year.png")
